98b71c8c9b3054edc29673fcaa52b42d69e2bfee
Alex Z <azalantai@williamhill.us>
2020-12-05 10:18:14 +0100

    Optimised

--- day5/aocD5.rb
+++ day5/aocD5.rb
@@ -1,16 +1,13 @@
-def task_one
+def day_five
   seat_id = []
   File.readlines('./aocD5.input').each do |line|
     line.gsub!(/[RLBF]/, 'R' => 1, 'L' => '0', 'B' => 1, 'F' => 0)
     row, seat = line[0, 7].to_i(2), line[7, 10].to_i(2)
     seat_id << row * 8 + seat
   end
-  seat_iD
+  puts "Max seat:", seat_id.max
+  #task_two
+  puts "My seat:", (seat_id.min..seat_id.max).to_a - seat_id
 end
 
-def task_two
-  (task_one.min..task_one.max).to_a - task_one
-end
-
-puts "Max seat:", task_one.max
-puts "My seat:", task_two
+day_five
