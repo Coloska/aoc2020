8450e788ca05459ffc38348826cc0714083933f7
Alex Z <azalantai@williamhill.us>
2020-12-06 13:56:41 +0100

    Revisits

--- day5/aocD5.rb
+++ day5/aocD5.rb
@@ -1,12 +1,7 @@
 def day_five
-  seat_id = []
-  File.readlines('./aocD5.input').each do |line|
-    line.gsub!(/[RLBF]/, 'R' => 1, 'L' => '0', 'B' => 1, 'F' => 0)
-    seat_id << line.to_i(2)
-  end
-  puts "Max seat:", seat_id.max
-  #task_two
-  puts "My seat:", (seat_id.min..seat_id.max).to_a - seat_id
+  input, seat_id = "./aocD5.input", []
+  File.readlines(input).each {|line| seat_id << line.gsub!(/[RLBF]/, 'R' => 1, 'L' => '0', 'B' => 1, 'F' => 0).to_i(2)}
+  p "Max seat:", seat_id.max , "My seat:", (seat_id.min..seat_id.max).to_a - seat_id
 end
 
 day_five
