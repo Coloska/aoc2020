59b6ddef49d0d472b812836e30b5fd62105d57f9
Alex Z <azalantai@williamhill.us>
2020-12-06 13:47:03 +0100

    Revisits

--- day5/aocD5.rb
+++ day5/aocD5.rb
@@ -2,7 +2,6 @@ def day_five
   seat_id = []
   File.readlines('./aocD5.input').each do |line|
     line.gsub!(/[RLBF]/, 'R' => 1, 'L' => '0', 'B' => 1, 'F' => 0)
-    row, seat = line[0, 7].to_i(2), line[7, 10].to_i(2)
     seat_id << line.to_i(2)
   end
   puts "Max seat:", seat_id.max
